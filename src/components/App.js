13626ba4c84b023a94078530d2dbc9d058430882
Anjan Layek <110993998+AnjanLayek@users.noreply.github.com>
2022-11-23 13:25:10 +0000

    aj

--- src/components/App.js
+++ src/components/App.js
@@ -7,12 +7,13 @@ class App extends React.Component {
 
   handleClick(){
     this.setState({count: this.state.count+1})
+    console.log( "rendered" )
   }
   constructor(props){
     super(props)
     this.state = {count:0}
     this.handleClick = this.handleClick.bind(this);
-
+     
   }
 
   render() {
